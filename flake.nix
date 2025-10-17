9721e05ab30b9da965600a6121f9d8a683b0288d
Tim Danner <tim@danners.us>
2025-10-16 20:11:48 -0500

    nix dev env

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "ASN.1 / SNMP MIB language server dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        inherit (pkgs) lib stdenv;
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages =
+            with pkgs;
+            [
+              rustToolchain
+              rust-analyzer
+              pkg-config
+              clang
+              cmake
+            ]
+            ++ lib.optionals stdenv.isDarwin [
+              libiconv
+            ];
+
+          env = {
+            RUST_BACKTRACE = "1";
+            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          };
+        };
+      }
+    );
+}
